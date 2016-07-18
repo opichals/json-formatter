583537ca1a29dab379d63ea92dab382168812dcb
Standa Opichal <opichals@gmail.com>
2016-07-18 21:15:03 +0200

    Trim trailing whitespace

--- extension/js/content.js
+++ extension/js/content.js
@@ -53,11 +53,11 @@
   // Open the port "jf" now, ready for when we need it
     // console.time('established port') ;
     port = chrome.extension.connect({name: 'jf'}) ;
-    
+
   // Add listener to receive response from BG when ready
     port.onMessage.addListener( function (msg) {
       // console.log('Port msg received', msg[0], (""+msg[1]).substring(0,30)) ;
-      
+
       switch (msg[0]) {
         case 'NOT JSON' :
           pre.hidden = false ;
@@ -65,7 +65,7 @@
           document.body.removeChild(jfContent) ;
           exitedNotJsonTime = +(new Date()) ;
           break ;
-          
+
         case 'FORMATTING' :
           isJsonTime = +(new Date()) ;
 
@@ -73,7 +73,7 @@
 
           // Clear the slowAnalysisTimeout (if the BG worker had taken longer than 1s to respond with an answer to whether or not this is JSON, then it would have fired, unhiding the PRE... But now that we know it's JSON, we can clear this timeout, ensuring the PRE stays hidden.)
             clearTimeout(slowAnalysisTimeout) ;
-          
+
           // Insert CSS
             jfStyleEl = document.createElement('style') ;
             jfStyleEl.id = 'jfStyleEl' ;
@@ -84,7 +84,7 @@
               'beforeend',
               'body{-webkit-user-select:text;overflow-y:scroll !important;margin:0;position:relative}#optionBar{-webkit-user-select:none;display:block;position:absolute;top:9px;right:17px}#buttonJSONApi,#buttonFormatted,#buttonPlain{-webkit-border-radius:2px;-webkit-box-shadow:0px 1px 3px rgba(0,0,0,0.1);-webkit-user-select:none;background:-webkit-linear-gradient(#fafafa, #f4f4f4 40%, #e5e5e5);border:1px solid #aaa;color:#444;font-size:12px;margin-bottom:0px;min-width:4em;padding:3px 0;position:relative;z-index:10;display:inline-block;width:80px;text-shadow:1px 1px rgba(255,255,255,0.3)}#buttonJSONApi{margin-left:0;border-top-left-radius:0;border-bottom-left-radius:0}#buttonFormatted{margin-left:0;border-top-left-radius:0;border-bottom-left-radius:0}#buttonPlain{margin-right:0;border-top-right-radius:0;border-bottom-right-radius:0;border-right:none}#buttonJSONApi:hover,buttonFormatted:hover,#buttonPlain:hover{-webkit-box-shadow:0px 1px 3px rgba(0,0,0,0.2);background:#ebebeb -webkit-linear-gradient(#fefefe, #f8f8f8 40%, #e9e9e9);border-color:#999;color:#222}#buttonJSONApi:active,#buttonFormatted:active,#buttonPlain:active{-webkit-box-shadow:inset 0px 1px 3px rgba(0,0,0,0.2);background:#ebebeb -webkit-linear-gradient(#f4f4f4, #efefef 40%, #dcdcdc);color:#333}#buttonJSONApi.selected,#buttonFormatted.selected,#buttonPlain.selected{-webkit-box-shadow:inset 0px 1px 5px rgba(0,0,0,0.2);background:#ebebeb -webkit-linear-gradient(#e4e4e4, #dfdfdf 40%, #dcdcdc);color:#333}#buttonJSONApi:focus,#buttonFormatted:focus,#buttonPlain:focus{outline:0}#jsonpOpener,#jsonpCloser{padding:4px 0 0 8px;color:#000;margin-bottom:-6px}#jsonpCloser{margin-top:0}#formattedJson{padding-left:28px;padding-top:6px}pre{padding:36px 5px 5px 5px}.kvov{display:block;padding-left:20px;margin-left:-20px;position:relative}.collapsed{white-space:nowrap}.collapsed>.blockInner{display:none}.collapsed>.ell:after{content:"…";font-weight:bold}.collapsed>.ell{margin:0 4px;color:#888}.collapsed .kvov{display:inline}.e{width:20px;height:18px;display:block;position:absolute;left:-2px;top:1px;z-index:5;background-image:url("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAgAAAAICAYAAADED76LAAAAGXRFWHRTb2Z0d2FyZQBBZG9iZSBJbWFnZVJlYWR5ccllPAAAAD1JREFUeNpiYGBgOADE%2F3Hgw0DM4IRHgSsDFOzFInmMAQnY49ONzZRjDFiADT7dMLALiE8y4AGW6LoBAgwAuIkf%2F%2FB7O9sAAAAASUVORK5CYII%3D");background-repeat:no-repeat;background-position:center center;display:block;opacity:0.15}.collapsed>.e{-webkit-transform:rotate(-90deg);width:18px;height:20px;left:0px;top:0px}.e:hover{opacity:0.35}.e:active{opacity:0.5}.collapsed .kvov .e{display:none}.blockInner{display:block;padding-left:24px;border-left:1px dotted #bbb;margin-left:2px}#formattedJson,#jsonpOpener,#jsonpCloser{color:#333;font:13px/18px monospace}#formattedJson{color:#444}.b{font-weight:bold}.s{color:#0B7500;word-wrap:break-word}a:link,a:visited{text-decoration:none;color:inherit}a:hover,a:active{text-decoration:underline;color:#050}.bl,.nl,.n{font-weight:bold;color:#1A01CC}.k{color:#000}#formattingMsg{font:13px "Lucida Grande","Segoe UI","Tahoma";padding:10px 0 0 8px;margin:0;color:#333}#formattingMsg>svg{margin:0 7px;position:relative;top:1px}[hidden]{display:none !important}span{white-space:pre-wrap}@-webkit-keyframes spin{from{-webkit-transform:rotate(0deg)}to{-webkit-transform:rotate(360deg)}}#spinner{-webkit-animation:spin 2s 0 infinite}*{-webkit-font-smoothing:antialiased}'
             ) ;
-  
+
             // Add custom font name if set - FROM FUTURE
               // if (typeof settings.fontName === 'string') {
               //   jfStyleEl.insertAdjacentHTML(
@@ -105,13 +105,13 @@
             setTimeout(function(){
               formattingMsg.hidden = false ;
             }, 250) ;
-          
-          
+
+
           // Create option bar
             var optionBar = document.createElement('div') ;
             optionBar.id = 'optionBar' ;
-          
-          
+
+
           // Show options link, if needed - FROM FUTURE
             // if (settings.enableOptionsLink) {
             //   var optionsLink = document.createElement('a') ;
@@ -121,7 +121,7 @@
             //   optionsLink.target = '_BLANK' ;
             //   optionBar.appendChild(optionsLink) ;
             // }
-          
+
           // Create toggleFormat button
             var buttonPlain = document.createElement('button'),
               buttonFormatted = document.createElement('button'),
@@ -133,7 +133,7 @@
             buttonJSONApi.id = 'buttonJSONApi' ;
             buttonJSONApi.innerText = 'JSONApi' ;
             buttonJSONApi.classList.add('selected') ;
-            
+
             buttonPlain.addEventListener(
               'click',
               function () {
@@ -150,7 +150,7 @@
               },
               false
             ) ;
-            
+
             buttonFormatted.addEventListener(
               'click',
               function () {
@@ -167,7 +167,7 @@
               },
               false
             ) ;
-            
+
             buttonJSONApi.addEventListener(
               'click',
               function () {
@@ -196,16 +196,16 @@
               generalClick,
               false // No need to propogate down
             ) ;
-          
+
           // Put option bar in DOM
             document.body.insertBefore(optionBar, pre) ;
 
           break ;
-            
+
         case 'FORMATTED' :
           // Insert HTML content
             jfContent.innerHTML = msg[1] ;
-          
+
           displayedFormattedJsonTime = Date.now() ;
 
           // Log times
@@ -218,26 +218,26 @@
           // Export parsed JSON for easy access in console
             setTimeout(function () {
               var script = document.createElement("script") ;
-              script.innerHTML = 'window.json = ' + msg[2] + ';' +
+              script.innerHTML = 'window.json = ' + msg[2] + ';'
                                  'document.getElementById("jsonapiContent").innerHTML = renderJSON(json);';
               document.head.appendChild(script) ;
               console.log('JSON Formatter: Type "json" to inspect.') ;
             }, 100) ;
 
           break ;
-        
+
         default :
           throw new Error('Message not understood: ' + msg[0]) ;
       }
     });
-  
+
     // console.timeEnd('established port') ;
 
 
   function ready () {
-    
+
     domReadyTime = Date.now() ;
-      
+
     // First, check if it's a PRE and exit if not
       var bodyChildren = document.body.childNodes ;
       pre = bodyChildren[0] ;
@@ -252,20 +252,20 @@
 
         // Disconnect the port (without even having used it)
           port.disconnect() ;
-        
+
         // EXIT POINT: NON-PLAIN-TEXT PAGE (or longer than 3MB)
       }
       else {
         // This is a 'plain text' page (just a body with one PRE child).
         // It might be JSON/JSONP, or just some other kind of plain text (eg CSS).
-        
+
         // Hide the PRE immediately (until we know what to do, to prevent FOUC)
           pre.hidden = true ;
           //console.log('It is text; hidden pre at ') ;
           slowAnalysisTimeout = setTimeout(function(){
             pre.hidden = false ;
           }, 1000) ;
-        
+
         // Send the contents of the PRE to the BG script
             jfContent = document.createElement('div') ;
             jfContent.id = 'jfContent' ;
@@ -305,7 +305,7 @@
         }
       });
   }
-  
+
   document.addEventListener("DOMContentLoaded", ready, false);
 
   var lastKvovIdGiven = 0 ;
@@ -366,7 +366,7 @@
 
     if (ev.which === 1) {
       var elem = ev.target ;
-      
+
       if (elem.className === 'e') {
         // It's a click on an expander.
 
@@ -378,7 +378,7 @@
             scrollTop = document.body.scrollTop,
             parentSiblings
         ;
-        
+
         // Expand or collapse
           if (parent.classList.contains('collapsed')) {
             // EXPAND
@@ -412,10 +412,10 @@
             }
 
           // console.log('Scrolltop HAS changed. document.body.scrollTop is now '+document.body.scrollTop+'; was '+scrollTop) ;
-          
+
           // The body has got a bit shorter.
           // We need to increase the body height by a bit (by increasing the bottom margin on the jfContent div). The amount to increase it is whatever is the difference between our previous scrollTop and our new one.
-          
+
           // Work out how much more our target scrollTop is than this.
             var difference = scrollTop - document.body.scrollTop  + 8 ; // it always loses 8px; don't know why
 
@@ -425,7 +425,7 @@
 
           // Now change the scrollTop back to what it was
             document.body.scrollTop = scrollTop ;
-            
+
         return ;
       }
     }
